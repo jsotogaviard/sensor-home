47b3a1125459fd1426be80da8f5eb0d86988699d
Jonathan <jonathan.Soto@polestar.eu>
2021-03-11 17:56:59 +0100

    more

--- sensor_reader/sensor_reader.js
+++ sensor_reader/sensor_reader.js
@@ -2,9 +2,6 @@ import noble from '@abandonware/noble'
 
 console.log("hello world")
 
-const services = await peripheral.discoverServicesAsync();
-console.log(services)
-
 noble.on('stateChange', function (state) {
     if (state === 'poweredOn') {
         console.log("poweredOn")
@@ -16,27 +13,15 @@ noble.on('stateChange', function (state) {
   });
   
   noble.on('discover', function (peripheral) {
-    console.log(`peripheral discovered (${peripheral.id} with address <${peripheral.address}, ${peripheral.addressType}>, connectable ${peripheral.connectable}, RSSI ${peripheral.rssi}:`);
-    console.log('\thello my local name is:');
-    console.log(`\t\t${peripheral.advertisement.localName}`);
-    console.log('\tcan I interest you in any of the following advertised services:');
-    console.log(`\t\t${JSON.stringify(peripheral.advertisement.serviceUuids)}`);
-  
-    const serviceData = peripheral.advertisement.serviceData;
-    if (serviceData && serviceData.length) {
-      console.log('\there is my service data:');
-      for (const i in serviceData) {
-        console.log(`\t\t${JSON.stringify(serviceData[i].uuid)}: ${JSON.stringify(serviceData[i].data.toString('hex'))}`);
-      }
-    }
-    if (peripheral.advertisement.manufacturerData) {
-      console.log('\there is my manufacturer data:');
-      console.log(`\t\t${JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex'))}`);
-    }
-    if (peripheral.advertisement.txPowerLevel !== undefined) {
-      console.log('\tmy TX power level is:');
-      console.log(`\t\t${peripheral.advertisement.txPowerLevel}`);
-    }
-  
-    console.log();
+    manufacturer_service_data_hex = peripheral.advertisement.serviceData[i].data.toString('hex') //a4c138ab590500a53f550b9b72" //mac a4c138ab5905
+    thermometer = {}
+    thermometer.id = peripheral.id // id == mac
+    thermometer.uuid = serviceData[0].uuid //"181a"
+    thermometer.mac =  manufacturer_service_data_hex.substring(0,12)
+    thermometer.temperature = int(manufacturer_service_data_hex(12,12 + 4), 16) / 10
+    thermometer.humidity =  int(manufacturer_service_data_hex(12 + 4,12 + 4 + 2), 16)
+    thermometer.battery_percentage =  int(manufacturer_service_data_hex(12 + 4 + 2,12 + 4 + 2 + 2), 16)
+    thermometer.battery_millivolts = int(manufacturer_service_data_hex(12 + 4 + 2 + 2,12 + 4 + 2 + 2 + 4), 16)
+    thermometer.counter =  int(manufacturer_service_data_hex(12 + 4 + 2 + 2 + 4,12 + 4 + 2 + 2 + 4 + 2), 16)
+    console.log(JSON.stringify(thermometer))
   });
