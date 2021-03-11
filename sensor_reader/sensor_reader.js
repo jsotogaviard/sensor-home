83bda4b56225d7da240f3f0c16208f39f9d8396f
Jonathan <jonathan.Soto@polestar.eu>
2021-03-11 18:01:38 +0100

    more

--- sensor_reader/sensor_reader.js
+++ sensor_reader/sensor_reader.js
@@ -19,10 +19,10 @@ noble.on('stateChange', function (state) {
     thermometer.id = peripheral.id // id == mac
     thermometer.uuid = serviceData[0].uuid //"181a"
     thermometer.mac =  manufacturer_service_data_hex.substring(0,12)
-    thermometer.temperature = int(manufacturer_service_data_hex(12,12 + 4), 16) / 10
-    thermometer.humidity =  int(manufacturer_service_data_hex(12 + 4,12 + 4 + 2), 16)
-    thermometer.battery_percentage =  int(manufacturer_service_data_hex(12 + 4 + 2,12 + 4 + 2 + 2), 16)
-    thermometer.battery_millivolts = int(manufacturer_service_data_hex(12 + 4 + 2 + 2,12 + 4 + 2 + 2 + 4), 16)
-    thermometer.counter =  int(manufacturer_service_data_hex(12 + 4 + 2 + 2 + 4,12 + 4 + 2 + 2 + 4 + 2), 16)
+    thermometer.temperature = parseInt(Number(manufacturer_service_data_hex(12,12 + 4), 16)) / 10
+    thermometer.humidity =  parseInt(Number(manufacturer_service_data_hex(12 + 4,12 + 4 + 2)), 16)
+    thermometer.battery_percentage =  parseInt(Number(manufacturer_service_data_hex(12 + 4 + 2,12 + 4 + 2 + 2)), 16)
+    thermometer.battery_millivolts = parseInt(Number(manufacturer_service_data_hex(12 + 4 + 2 + 2,12 + 4 + 2 + 2 + 4)), 16)
+    thermometer.counter =  parseInt(Number(manufacturer_service_data_hex(12 + 4 + 2 + 2 + 4,12 + 4 + 2 + 2 + 4 + 2)), 16)
     console.log(JSON.stringify(thermometer))
   });
