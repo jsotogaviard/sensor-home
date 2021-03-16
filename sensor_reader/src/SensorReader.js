b3e295bca8fec7e4985ecbf483b27d3a407d3e11
Jonathan Soto <jsotogaviard@gmail.com>
2021-03-16 21:25:58 +0100

    more

--- sensor_reader/src/SensorReader.js
+++ sensor_reader/src/SensorReader.js
@@ -19,7 +19,7 @@ export function isKnownSensor(peripheral) {
 
 export function getKnownSensors(sensors) {
   const knownSensors = []
-  sensors.forEach(sensor => {
+  sensors.forEach((sensor,key) => {
     if(isKnownSensor(sensor)){
       const knownSensor = parseAdvData(sensor)
       knownSensors.push(knownSensor)
