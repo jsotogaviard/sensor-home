80ae712e84b360f639c41ece47817d3e3c61f778
Jonathan <jonathan.Soto@polestar.eu>
2021-03-31 10:32:57 +0200

    more

--- sensor_reader/src/main.js
+++ sensor_reader/src/main.js
@@ -31,4 +31,6 @@ setTimeout(() => {
   const knownSensors = getKnownSensors(sensors)
   write2influx(knownSensors)
 
+  process.exit(1)
+
 }, process.env.SCANNING_TIME_MS || 5000); // TODO add env variable for scanning time
