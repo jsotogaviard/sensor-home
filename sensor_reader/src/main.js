c1bb88cc5b116fc312cf65af2c1f309e94fe0230
Jonathan <jonathan.Soto@polestar.eu>
2021-04-01 16:54:47 +0200

    more

--- sensor_reader/src/main.js
+++ sensor_reader/src/main.js
@@ -30,7 +30,7 @@ setTimeout(async () => {
 
   // Retrieve known sensors and write them in influx
   const knownSensor = sensors["a4c138a8554e"]
-  if(knownSensor)
+  if (knownSensor)
     await explore(knownSensor)
 
 }, process.argv[2] || 5000); // TODO add env variable for scanning time
@@ -39,108 +39,19 @@ console.log(process.argv[2])
 async function explore(peripheral) {
   console.log('services and characteristics:' + JSON.stringify(peripheral.id));
 
-  peripheral.on('disconnect', function() {
+  peripheral.on('disconnect', function () {
     console.log("disconnect")
     process.exit(0);
   });
 
-  peripheral.connect(function(error) {
+  peripheral.connect(function (error) {
     console.log("connected")
-    peripheral.discoverServices([], function(error, services) {
-      console.log("discoverServices")
-      console.log(services.length)
+    peripheral.discoverAllServicesAndCharacteristics([], function (error, services, characteristics) {
+      console.log("discoverAllServicesAndCharacteristics")
+      console.log("Services " + services.length)
+      console.log("Characterisitcs " + characteristics.length)
+      console.log(characteristics)
       console.log(services)
-      var serviceIndex = 0;
-
-      async.whilst(
-        function () {
-          return (serviceIndex < services.length);
-        },
-        function(callback) {
-          var service = services[serviceIndex];
-          var serviceInfo = service.uuid;
-
-          if (service.name) {
-            serviceInfo += ' (' + service.name + ')';
-          }
-          console.log(serviceInfo);
-
-          service.discoverCharacteristics([], function(error, characteristics) {
-            var characteristicIndex = 0;
-
-            async.whilst(
-              function () {
-                return (characteristicIndex < characteristics.length);
-              },
-              function(callback) {
-                var characteristic = characteristics[characteristicIndex];
-                var characteristicInfo = '  ' + characteristic.uuid;
-
-                if (characteristic.name) {
-                  characteristicInfo += ' (' + characteristic.name + ')';
-                }
-
-                async.series([
-                  function(callback) {
-                    characteristic.discoverDescriptors(function(error, descriptors) {
-                      async.detect(
-                        descriptors,
-                        function(descriptor, callback) {
-                          if (descriptor.uuid === '2901') {
-                            return callback(descriptor);
-                          } else {
-                            return callback();
-                          }
-                        },
-                        function(userDescriptionDescriptor){
-                          if (userDescriptionDescriptor) {
-                            userDescriptionDescriptor.readValue(function(error, data) {
-                              if (data) {
-                                characteristicInfo += ' (' + data.toString() + ')';
-                              }
-                              callback();
-                            });
-                          } else {
-                            callback();
-                          }
-                        }
-                      );
-                    });
-                  },
-                  function(callback) {
-                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
-
-                    if (characteristic.properties.indexOf('read') !== -1) {
-                      characteristic.read(function(error, data) {
-                        if (data) {
-                          var string = data.toString('ascii');
-
-                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
-                        }
-                        callback();
-                      });
-                    } else {
-                      callback();
-                    }
-                  },
-                  function() {
-                    console.log(characteristicInfo);
-                    characteristicIndex++;
-                    callback();
-                  }
-                ]);
-              },
-              function(error) {
-                serviceIndex++;
-                callback();
-              }
-            );
-          });
-        },
-        function (err) {
-          peripheral.disconnect();
-        }
-      );
-    });
+    })
   })
 }
