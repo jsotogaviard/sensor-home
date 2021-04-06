0301d0a2481a8fef05f01f311c8cb0aad423ad83
Jonathan <jonathan.Soto@polestar.eu>
2021-04-06 14:17:49 +0200

    more

--- sensor_reader/src/main.js
+++ sensor_reader/src/main.js
@@ -9,7 +9,7 @@ const sensors = {}
 // Start scanning
 noble.on('stateChange', state => {
   if (state === 'poweredOn') {
-    noble.startScanning();
+    noble.startScanning([], true);
   } else {
     noble.stopScanning();
   }
@@ -48,10 +48,6 @@ noble.on('discover', peripheral => {
     scale.uknown2 = scale.control_bit_01[index--] == "1"
     scale.uknown1 = scale.control_bit_01[index--] == "1"
     scale.IsLBS = scale.control_bit_01[index--] == "1"
-    console.log(manufacturer_service_data_hex.substring(8, 10))
-    console.log(parseInt(manufacturer_service_data_hex.substring(8, 10), 16))
-    console.log(manufacturer_service_data_hex.substring(6, 8))
-    console.log(parseInt(manufacturer_service_data_hex.substring(6, 8), 16))
     scale.year = parseInt(manufacturer_service_data_hex.substring(8, 10)+manufacturer_service_data_hex.substring(6, 8),16)
     scale.month = parseInt(manufacturer_service_data_hex.substring(10, 12), 16)
     scale.day = parseInt(manufacturer_service_data_hex.substring(12, 14), 16)
